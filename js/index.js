2687d797752685edbdf00a327330f4189b05a19f
Fabio Niglio <fabio_niglio@hotmail.com>
2024-03-25 18:43:32 +0100

    health bar

--- js/index.js
+++ js/index.js
@@ -57,9 +57,11 @@ function animate() {
   requestAnimationFrame(animate);
 
   c.drawImage(image, 0, 0);
-  enemies.forEach((enemy) => {
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i];
+
     enemy.update();
-  });
+  }
 
   placementTiles.forEach((tile) => {
     tile.update(mouse);
@@ -76,7 +78,6 @@ function animate() {
       return distance < enemy.radius + building.radius;
     });
     building.target = validEnemies[0];
-    console.log(validEnemies);
 
     for (let i = building.projectiles.length - 1; i >= 0; i--) {
       const projectile = building.projectiles[i];
@@ -87,7 +88,17 @@ function animate() {
       const yDifference = projectile.enemy.center.y - projectile.position.y;
 
       const distance = Math.hypot(xDifference, yDifference);
+
+      // Projectile hits an enemy
       if (distance < projectile.enemy.radius + projectile.radius) {
+        projectile.enemy.health -= 20;
+        if (projectile.enemy.health <= 0) {
+          const enemyIndex = enemies.findIndex((enemy) => {
+            return projectile.enemy === enemy;
+          });
+          if (enemyIndex > -1) enemies.splice(enemyIndex, 1);
+        }
+        console.log(projectile.enemy.health);
         building.projectiles.splice(i, 1);
       }
     }
