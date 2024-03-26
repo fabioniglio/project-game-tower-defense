95699b630d5a0682a482154e574931f132a8c2dc
Fabio Niglio <fabio_niglio@hotmail.com>
2024-03-26 16:07:07 +0100

    speed change

--- js/classes/Enemy.js
+++ js/classes/Enemy.js
@@ -43,7 +43,7 @@ class Enemy extends Sprite {
     const xDistance = waypoint.x - this.center.x;
     const angle = Math.atan2(yDistance, xDistance);
 
-    const speed = 30;
+    const speed = 3;
     this.velocity.x = Math.cos(angle) * speed;
     this.velocity.y = Math.sin(angle) * speed;
 
